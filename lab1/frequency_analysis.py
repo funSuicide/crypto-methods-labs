4156e8710174e7b4416e7df9ecf5a84980afc457
gwymlas <smrszr63@gmail.com>
2024-02-15 22:56:05 +0400

    Реализован частотный анализ

---
+++ lab1/frequency_analysis.py
@@ -0,0 +1,34 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+freq = [0.0817, 0.0149, 0.0278, 0.0425, 0.1270, 0.0223,
+        0.0202, 0.0609, 0.0697, 0.0015, 0.0077, 0.0403,
+        0.0241, 0.0675, 0.0751, 0.0193, 0.0010, 0.0599,
+        0.0633, 0.0906, 0.0276, 0.0098, 0.0236, 0.0015,
+        0.0197, 0.0007]
+
+freq_stats = dict(zip(list(alphabet), freq))
+
+
+def frequency_analysis(input_file, output_file):
+    symbols = dict.fromkeys(list(alphabet), 0)
+    counter = 0
+    for line in input_file:
+        for char in line:
+            if not (char == ' ' or char == '\n'):
+                symbols[char.lower()] += 1
+                counter += 1
+
+    for char in symbols.keys():
+        symbols[char] /= counter
+
+    sorted_symbols_freq = dict(sorted(symbols.items(), key=lambda item: item[1], reverse=True))
+    sorted_freq_stats = dict(sorted(freq_stats.items(), key=lambda item: item[1], reverse=True))
+
+    substitution = dict(zip(sorted_symbols_freq.keys(), sorted_freq_stats.keys()))
+    substitution['\n'] = '\n'
+    substitution[' '] = ' '
+
+    input_file.seek(0)
+    decrypt_text = ''.join([substitution[ch] for line in input_file for ch in line])
+    output_file.write(decrypt_text)
+
--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,14 @@
 from caesar_cipher import caesar_operation
+from frequency_analysis import frequency_analysis
 
 input_path = 'open_text.txt'
 output_path = 'cipher_text.txt'
+decrypt_text_path = 'decrypt_text.txt'
 
-input_file = open(input_path, 'r')
-output_file = open(output_path, 'w')
+open_file = open(input_path, 'r')
+cipher_file = open(output_path, 'w')
+decrypt_text = open(decrypt_text_path, 'w')
 
-caesar_operation(input_file, output_file, 3, 'e')
+# caesar_operation(open_file, cipher_file, 3, 'e')
+frequency_analysis(open_file, decrypt_text)
+
