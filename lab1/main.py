d2cd92de3f231ebdcc7bc90f30b419c16e062082
Admin <Admin@LAPTOP-U4DN8FG5>
2024-02-16 18:53:53 +0400

    добавлена возможность ручной модификации промежуточного текста

--- lab1/main.py
+++ lab1/main.py
@@ -4,11 +4,16 @@ from frequency_analysis import frequency_analysis
 input_path = 'open_text.txt'
 output_path = 'cipher_text.txt'
 decrypt_text_path = 'decrypt_text.txt'
+test_path = 'test.txt'
 
-open_file = open(input_path, 'r')
+open_file = open(test_path, 'r')
 cipher_file = open(output_path, 'w')
 decrypt_text = open(decrypt_text_path, 'w')
 
-# caesar_operation(open_file, cipher_file, 3, 'e')
-frequency_analysis(open_file, decrypt_text)
+caesar_operation(open_file, cipher_file, 3, 'e')
+
+cipher_file.close()
+cipher_file = open(output_path, 'r')
+
+frequency_analysis(cipher_file, decrypt_text)
 
