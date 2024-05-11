a4f3e79ab89783f431aa7538e0bc7d5e794c560e
funSuicide <Digital-Corsair@yandex.com>
2024-05-11 12:48:32 +0400

    ничего не работает, лаба - параша

--- lab4/main.py
+++ lab4/main.py
@@ -74,7 +74,7 @@ def get_bin_vector(m: int, par_p: int, par_h: int) -> list:
     M = []
     current_m = m
 
-    for i in range(1, par_p+1):
+    for i in range(1, par_p + 1):
         if current_m >= get_binomial_coefficient(par_p=par_p - i, par_h=current_l):
             M.append(1)
             current_m = current_m - get_binomial_coefficient(par_p=par_p - i, par_h=current_l)
@@ -92,11 +92,13 @@ def get_pi(par_p):
 
 
 def get_keys(par_p, par_h):
-    GF = galois.GF(7 ** 3, repr="poly")
-    f = galois.Poly.Str("2*x^3 + 1", field=GF)
-    g = galois.Poly.Str("3*x^3 + 5*x^2 + 2*x + 5", field=GF)  # опционально подумать все же над генерацией
+    GF = galois.GF(par_p ** par_h, repr="poly", irreducible_poly="x^4 + 3*x^3 + 5*x^2 + 6*x + 2")
+    f = galois.Poly.Str("x^4 + 3*x^3 + 5*x^2 + 6*x + 2", field=GF)
+    # g = galois.Poly.Str("3*x^3 + 3*x^2 + 6", field=GF)  # опционально подумать все же над генерацией
 
-    g = GF.primitive_elements[-1]  # надо подумать как выбрать именно наш, ну как варик по списку поискать просто
+    # g = GF.primitive_elements[-1]  # надо подумать как выбрать именно наш, ну как варик по списку поискать просто
+
+    g = GF("3*x^3 + 3*x^2 + 6")
 
     test = GF.Range(start=par_p, stop=par_p + par_p)
 
@@ -113,30 +115,44 @@ def get_keys(par_p, par_h):
     return OpenKey(par_p=par_p, par_h=par_h, par_c=Ci), CloseKey(par_fx=f, par_gx=g, par_pi=pi, par_d=d)
 
 
-def encrypt(m: int, A: OpenKey):
+def encrypt(m: int, A: OpenKey) -> int:
     en_p = A.p
     en_h = A.h
     en_c = A.c
 
-    #len_bin_str = math.floor(get_binomial_coefficient(par_p=en_p, par_h=en_h)) #чот пока не понял
-
     M = get_bin_vector(m=m, par_p=en_p, par_h=en_h)
-
-    '''
-    if len(M) != len_bin_str:
-        print('proebali')
-        return -1, 0
-        '''
 
     result_c = 0
     for i in range(0, en_p):
         result_c += (M[i] * en_c[i])
 
-    return 0, result_c % (pow(en_p, en_h) - 1)
+    return result_c % (pow(en_p, en_h) - 1)
 
-def decrypt():
-    return 0
 
+def decrypt(c: int, A: OpenKey, B: CloseKey) -> int:
+    de_h = A.h
+    de_d = B.d
+    de_p = A.p
+    de_fx = B.fx
+    de_gx = B.gx
+
+    r = (c - de_h * de_d) % (pow(de_p, de_h) - 1)
+
+    u_x = pow(de_fx, r, de_gx)
+
+    s_x = u_x + de_fx
+
+    Ti = []  # разложение нада
+
+    M = []  # получение бинарного вектора
+
+    m = 0
+    l = de_h
+
+    for i in range(1, p + 1):
+        m = 1  # восстановление m
+
+    return m
 
 
 def main():
@@ -190,8 +206,8 @@ def main():
     # s = generate_polynomial(7, 4)
     # print(s)
 
-    open_key, close_key = get_keys(par_p=7, par_h=3)
-    _, en_data = encrypt(22, open_key)
+    open_key, close_key = get_keys(par_p=7, par_h=4)
+    en_data = encrypt(22, open_key)
     print(en_data)
 
 
